9290cd2fa6b2dccac6d8a5757eab48a7ddcf2be5
nikhil11001 <nikhilgade11001@gmail.com>
2021-09-05 16:49:49 +0530

    Hide menu bar on menu click

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -1,27 +1,35 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 export const NavBar = () => {
+  const collapseBtnRef = React.createRef();
+  const basicNavRef = React.createRef();
+
+  const hideMenu = () => {
+    collapseBtnRef.current.classList.add("collapsed");
+    basicNavRef.current.classList.remove("show");
+  };
+
   return (
     <Navbar bg="light" expand="lg" fixed="top">
       <Container>
         <Navbar.Brand>PROPIUSS</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
+        <Navbar.Toggle aria-controls="basic-navbar-nav" ref={collapseBtnRef} />
+        <Navbar.Collapse id="basic-navbar-nav" ref={basicNavRef}>
           <Nav className="me-auto">
-            <Nav.Link>
+            <Nav.Link onClick={hideMenu}>
               <Link to="/home">HOME</Link>
             </Nav.Link>
 
-            <Nav.Link>
+            <Nav.Link onClick={hideMenu}>
               <Link to="/package">PACKAGE</Link>
             </Nav.Link>
-            <Nav.Link>
+            <Nav.Link onClick={hideMenu}>
               <Link to="/about">ABOUT US</Link>
             </Nav.Link>
 
-            <Nav.Link>
+            <Nav.Link onClick={hideMenu}>
               <Link to="/contact">CONTACT</Link>
             </Nav.Link>
           </Nav>
