f9a4112414aaa4a5544c01db5fe0f306bd916787
nikhil11001 <nikhilgade11001@gmail.com>
2021-09-10 23:19:31 +0530

    Added css file in index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { AppRouter } from "./routes/appRouter";
+import "./App.css";
 
 ReactDOM.render(
   <React.StrictMode>
